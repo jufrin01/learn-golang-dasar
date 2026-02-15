0f44b2f1128afa05461f3a543fddf6f0943c4527
jufrin01 <jufrinaha111@gmail.com>
2026-02-15 11:22:37 +0700

    learn golang part 5

---
+++ learngo/05-write-your-first-library-package/exercise/solution/golang/cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt" // You should replace this with your username
+	"github.com/inancgumus/learngo/05-write-your-first-library-package/exercise/solution/golang"
+)
+
+func main() {
+	fmt.Println(golang.Version())
+}
