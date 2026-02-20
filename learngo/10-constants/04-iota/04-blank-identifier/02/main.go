ad5a6240b0c91c092243f82c150a0cc8ef8e6db0
jufrin01 <jufrinaha111@gmail.com>
2026-02-21 05:17:43 +0700

    part 10

---
+++ learngo/10-constants/04-iota/04-blank-identifier/02/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		EST = -(5 + iota) // CORRECT: -5
+		MST               // CORRECT: -6
+		PST               // CORRECT: -7
+	)
+
+	fmt.Println(EST, MST, PST)
+}
